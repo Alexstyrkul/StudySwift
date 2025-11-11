98d58e72dc6b15c64c76bf614979bc7e1da19d78
Oleksandr Styrkul <alexroast3@gmail.com>
2025-11-11 18:11:59 +0100

    Fix: convert iOSSampleAppUITests from submodule to normal folder

---
+++ iOSSampleApp-master/Sources/iOSSampleAppUITests/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+var name = "Alex"
+print ("my name is \(name)")
+
+name = "Styrkul"
+print ("Now my name is \(name)")
+       
+   let country = "Ukraine"
+print("I am from \(country)")
+// lession 2
+
+var SecondName: String = "Styrkul"
+var Sity: String = "Odesa"
+var age: Int = 25
+var experience: Double = 3.5
+var IsAutomations: Bool = false
+print ("My name is \(name) i living in \(Sity) and i am \(age) my experience is \(experience) years and i work in \(IsAutomations)")
